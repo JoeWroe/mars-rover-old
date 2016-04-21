9f550213dd8c3afdd5b331ed00bcdb5bda34e31b
JoeWroe <joe_wroe@icloud.com>
2016-04-21 20:02:08 +0200

    Refactor MarsRover unit tests
    ∆ Exchange 'subject' for a named subject. This improves readability of
    test suit.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,7 +2,9 @@ require 'mars_rover'
 
 describe MarsRover do
 
+  subject(:mars_rover) { described_class.new }
+
   it 'responds with "true" when MarsRover is #online' do
-    expect(subject.online).to eq(true)
+    expect(mars_rover.online).to eq(true)
   end
 end
