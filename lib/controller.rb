5cfff8a7f195026e2faf07ea89c03c59d203a4bb
JoeWroe <joe_wroe@icloud.com>
2016-04-24 12:24:41 +0200

    Refactor Controller class
    ∆ Refactor argument in #online? from 'mars_rover' to 'vehicle'. This
    is ab advantage of dependency injection, the Controller class can check
    the state of any object that responds to #online?. Therefore, in future
    advances of the program, another designer may want the controller to
    check state of other objects such as landing pods or scientific
    equipment. The method as it is now set up provides easily changable
    code to facilitate this.

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,8 +2,8 @@ require_relative 'mars_rover'
 
 class Controller
 
-  def online?(mars_rover)
-    mars_rover.online
+  def online?(vehicle)
+    vehicle.online
   end
 
   def land(vehicle, location)
