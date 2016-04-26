fe93889a557165c33edc5dbd26bd9eb737708f18
JoeWroe <joe_wroe@icloud.com>
2016-04-26 15:45:40 +0200

    Refactor Controller tests
    ∆ Start setting up doubles, all calls to other classes have now been
    DRY'd up into let blocks at start of class. Still needs to be acctual
    doubles. For now this just shortens tests making them more readable.

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -4,6 +4,9 @@ describe Controller do
 
   subject(:controller) { described_class.new}
 
+  let(:mars_rover) { MarsRover.new }
+  let(:plateau)    { Plateau.new }
+
   describe 'checking and changing the #online state of an object' do
 
     it 'can question if an object is #online?' do
@@ -14,8 +17,6 @@ describe Controller do
   describe 'landing objects' do
 
     it 'can land an object' do
-      mars_rover = MarsRover.new
-      plateau    = Plateau.new
       controller.land(mars_rover, plateau)
       expect(mars_rover.landed(plateau)).to eq(true)
     end
@@ -28,35 +29,35 @@ describe Controller do
     end
 
     it 'can switch a vehicles recording on' do
-      mars_rover = MarsRover.new
       controller.toggle_recording(mars_rover)
       expect(mars_rover.recording).to eq(true)
     end
 
     it 'can switch a vehicles recording off' do
-      mars_rover = MarsRover.new
       controller.toggle_recording(mars_rover)
       controller.toggle_recording(mars_rover)
       expect(mars_rover.recording).to eq(false)
     end
   end
 
-  describe 'checking a vehicles position' do
+  describe 'managing a vehicles position' do
 
     it 'can question the position of an object' do
-      expect(controller).to respond_to(:vehicle_position).with(1).argument
+      expect(controller.vehicle_position(mars_rover)).to eq(mars_rover.current_position)
+    end
+
+    it 'can rotate a vehicle right' do
+
     end
   end
 
   describe 'managing a navigation grid for a plateau' do
 
     it 'can check the maximum boundries of the grid' do
-      plateau = Plateau.new
       expect(controller.check_location_bounds(plateau)).to eq(plateau.grid_size)
     end
 
     it 'can #add_navigation_grid' do
-      plateau = Plateau.new
       controller.add_navigation_grid(plateau, 5, 5)
       expect(controller.check_location_bounds(plateau)).to eq("Maximum point on x-axis is 5.\nMaximum point on y-axis is 5.")
     end
